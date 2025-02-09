d1aead3ba59ea2ccd7a47064ea2d07747a3fdbf4
lakshaykhokhar2003 <lakshaykhokhar2003@gmail.com>
2025-02-10 00:05:20 +0530

    fix: build error

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
 import CalendarDemo from '@/components/calendar-demo'
+import {API} from "@/lib/utils";
 
 export default function Home() {
+  console.log(`${API}/api/events/`)
+
   return <CalendarDemo />
 }
